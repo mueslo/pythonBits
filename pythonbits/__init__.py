40e77421502954924bd463f3b0bd0eb3cc99c2da
mueslo <johannesfalke@googlemail.com>
2018-02-08 04:48:07 +0100

    Bump version to beta 3

--- pythonbits/__init__.py
+++ pythonbits/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = "pythonBits"
-__version__ = "3.0b2"
+__version__ = "3.0b3"
 __copyright__ = "Copyright 2018, The pythonBits Authors"
 __maintainer__ = "mueslo"
 __license__ = "GPLv3"
