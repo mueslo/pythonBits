0eea4a07d8a4a913f75d615574103b9e94a04785
mueslo <johannesfalke@googlemail.com>
2018-02-08 04:14:33 +0100

    Rating fixes and prettification

--- pythonbits/__init__.py
+++ pythonbits/__init__.py
@@ -3,6 +3,8 @@
 __title__ = "pythonBits"
 __version__ = "3.0b2"
 __copyright__ = "Copyright 2018, The pythonBits Authors"
+__maintainer__ = "mueslo"
 __license__ = "GPLv3"
 
 _release = __title__ + " " + __version__
+_github = 'https://github.com/' + __maintainer__ + '/' + __title__
