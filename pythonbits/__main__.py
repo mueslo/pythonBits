20e39090f5beaa1851d472b100356c9bda8b9a87
mueslo <johannesfalke@googlemail.com>
2018-02-15 20:18:42 +0100

    Fix accidental argparse exception catching

--- pythonbits/__main__.py
+++ pythonbits/__main__.py
@@ -100,9 +100,7 @@ def parse_args():
     return Category, set_field, get_field
 
 
-def main():
-    Category, set_fields, get_fields = parse_args()
-
+def main(Category, set_fields, get_fields):
     # todo: first try show_fields, if it raises attributeerror, categorise
     sub = Category(**set_fields)
     sub = sub.categorise()
@@ -120,7 +118,8 @@ def main():
 
 
 if __name__ == '__main__':
+    Category, set_fields, get_fields = parse_args()
     with logging.log.catch_exceptions(
             "An exception occured.\nFull log stored at file://{}",
             logging.LOG_FILE):
-        main()
+        main(Category, set_fields, get_fields)
